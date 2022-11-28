868222beca565e759b343d828f09f57869124e3d
Jason Kubo <kuboj86@gmail.com>
2022-11-28 00:44:11 -0500

    Add project files.

---
+++ Models/DepartmentLocation.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeesCh12.Models
+{
+    public class DepartmentLocation
+    {
+        public int DepartmentID { get; set; }
+        public int LocationID { get; set; }
+
+        public Department Department { get; set; }
+        public Location Location { get; set; }
+    }
+}
